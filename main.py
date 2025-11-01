31aae3ff6fde8910d4d7ad39349c6131cd2c299e
Peter Amerkhanian <peteamer@gmail.com>
2025-11-01 13:55:53 -0700

    fixed some regex issues

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from email_tools import parse_email, gmail_con
+import pandas as pd
 
 
 def fetch_latest_emails(service, max_results=10):
@@ -14,15 +15,22 @@ def fetch_latest_emails(service, max_results=10):
         msg_detail = service.users().messages().get(userId='me', id=msg['id']).execute()
         payload = msg_detail.get('payload', {})
         headers = payload.get('headers', [])
+        sender = next((h['value'] for h in headers if h['name'] == 'From'), '(Unknown Sender)')
         subject = next((h['value'] for h in headers if h['name'] == 'Subject'), '(No Subject)')
         date = next((h['value'] for h in headers if h['name'] == 'Date'), '(No Date)')
         msg_body = parse_email.parse_email_body(service, msg['id'])
+        snippet = msg_detail.get('snippet', '')
+        bill_amounts = list(set(
+            parse_email.extract_amounts(msg_body) +
+            parse_email.extract_amounts(snippet)
+        ))
         email_data.append({
+            'sender': sender,
             'subject': subject,
             'date': date,
             'snippet': msg_detail.get('snippet', ''),
             'body': msg_body,
-            'amounts': parse_email.extract_amounts(msg_body)
+            'amounts': bill_amounts
         })
     return email_data
 
@@ -30,9 +38,12 @@ def fetch_latest_emails(service, max_results=10):
 if __name__ == '__main__':
     service = gmail_con.get_gmail_service()
     emails = fetch_latest_emails(service, max_results=10)
+    emails_df = pd.DataFrame(emails)
+    emails_df.to_csv('data/test_output.csv', index=False)
     for email in emails:
+        print(f"Sender: {email['sender']}")
         print(f"Subject: {email['subject']}")
         print(f"Date: {email['date']}")
         print(f"Snippet: {email['snippet']}")
-        print(f"Body: {email['body'][:20]}...")
+        print(f"Body: {email['body'][:80]}...")
         print(f"Amounts: {email['amounts']}\n{'-'*40}")
