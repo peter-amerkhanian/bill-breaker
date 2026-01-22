2fc6f6a0fa533d236cfe8325877d97c266a4c1f3
Peter Amerkhanian <peteamer@gmail.com>
2026-01-22 09:10:21 -0800

    working version w/ limited data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from email_tools import parse_email, gmail_con
 import pandas as pd
+from email.utils import parseaddr, parsedate_to_datetime
 
 
 def fetch_latest_emails(service, max_results=10):
@@ -15,9 +16,11 @@ def fetch_latest_emails(service, max_results=10):
         msg_detail = service.users().messages().get(userId='me', id=msg['id']).execute()
         payload = msg_detail.get('payload', {})
         headers = payload.get('headers', [])
-        sender = next((h['value'] for h in headers if h['name'] == 'From'), '(Unknown Sender)')
+        sender_raw = next((h['value'] for h in headers if h['name'] == 'From'), '(Unknown Sender)')
+        sender_name, sender_email = parseaddr(sender_raw)
         subject = next((h['value'] for h in headers if h['name'] == 'Subject'), '(No Subject)')
-        date = next((h['value'] for h in headers if h['name'] == 'Date'), '(No Date)')
+        date_raw = next((h['value'] for h in headers if h['name'] == 'Date'), '(No Date)')
+        date = parsedate_to_datetime(date_raw)
         msg_body = parse_email.parse_email_body(service, msg['id'])
         snippet = msg_detail.get('snippet', '')
         bill_amounts = list(set(
@@ -25,10 +28,11 @@ def fetch_latest_emails(service, max_results=10):
             parse_email.extract_amounts(snippet)
         ))
         email_data.append({
-            'sender': sender,
+            'sender_name': sender_name,
+            'sender_email': sender_email,
             'subject': subject,
             'date': date,
-            'snippet': msg_detail.get('snippet', ''),
+            'snippet': snippet,
             'body': msg_body,
             'amounts': bill_amounts
         })
